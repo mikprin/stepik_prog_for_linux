3862648bcea754b517026dddcf02105b7ddbea21
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2021-10-21 15:27:28 +0300

    WHRAAY lw

--- l2/main.c
+++ l2/main.c
@@ -3,11 +3,12 @@
 int main()
 {
     //hello_message("USER");
-    const char s[] = "1231231231";
+    const char s[] = "";
     size_t multiplier = 1;
     int count = 1;
     int ret_code = stringStat(&s, multiplier, &count);
 
     printf("%d \n",ret_code);
+    printf("%d \n",count);
     return 0;
 }
--- l2/solution.c
+++ l2/solution.c
@@ -1,12 +1,14 @@
+#include <stddef.h>
+
 #include "solution.h"
 
-#include <stddef.h>
+
 #include <stdio.h>
 #include <string.h>
 
 int stringStat(const char *string, size_t multiplier, int *count){
     int str_lenth_modified = multiplier * strlen(string);  // Return to function
-    count += 1; // Iterate count
+    (*count)++;  // Iterate count
     return str_lenth_modified; // return value
 }
 
