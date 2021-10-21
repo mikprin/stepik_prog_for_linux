8f02c698b85cce4bc781ac16e4c7393dfa49acdb
Mikhail Solovyanov <solovyanov.mm@phystech.edu>
2021-10-21 14:28:39 +0300

    still faild

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # stepik_prog_for_linux
 stepik_prog_for_linux
+
+
+
+## Usefull commans
+
+Проверить от чего зависит библиотека или бинарный файл:
+
+`ldd my_obj`
--- l2/solution.c
+++ l2/solution.c
@@ -1,5 +1,9 @@
 #include "solution.h"
 
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
 int stringStat(const char *string, size_t multiplier, int *count){
     int str_lenth_modified = multiplier * strlen(string);  // Return to function
     count += 1; // Iterate count
